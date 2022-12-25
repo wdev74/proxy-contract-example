9eee77b3a07bfb5d70486f80dd6226b8d08673e2
wdev74 <webdeveloper0704@gmail.com>
2022-12-25 15:21:42 -0800

    Fixed esLint and prettier

--- contracts/BoxV2.sol
+++ contracts/BoxV2.sol
@@ -3,10 +3,10 @@
 pragma solidity ^0.8.0;
 
 import "./Box.sol";
- 
-contract BoxV2 is Box{
+
+contract BoxV2 is Box {
     // Increments the stored value by 1
     function increment() public {
-        store(retrieve()+1);
+        store(retrieve() + 1);
     }
 }
--- contracts/BoxV3.sol
+++ contracts/BoxV3.sol
@@ -3,11 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "./BoxV2.sol";
- 
-contract BoxV3 is BoxV2{
+
+contract BoxV3 is BoxV2 {
     string public name;
- 
+
     event NameChanged(string name);
+
     function setName(string memory _name) public {
         name = _name;
         emit NameChanged(name);
